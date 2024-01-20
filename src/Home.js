673c685fd6135e4af2375a796b307fbdc6e200cf
PradhiveAdya <pradhive.baskaran@adya.ai>
2024-01-20 19:19:10 +0530

    changes

--- src/Home.js
+++ src/Home.js
@@ -1,50 +1,53 @@
-import { useRef, useEffect } from "react";
-import gsap from "gsap";
-import { ScrollTrigger } from "gsap/all";
-import "./Home.css";
+import { useRef, useEffect } from 'react';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
+import "./Home.css"
 
 export default function Home() {
+
   const firstText = useRef(null);
   const secondText = useRef(null);
   const slider = useRef(null);
   let xPercent = 0;
   let direction = -1;
 
-  useEffect(() => {
+  useEffect( () => {
     gsap.registerPlugin(ScrollTrigger);
     gsap.to(slider.current, {
       scrollTrigger: {
         trigger: document.documentElement,
-        scrub: 0.25,
+        scrub:  0.075,
         start: 0,
         end: window.innerHeight,
-        onUpdate: (e) => (direction = e.direction * -1),
+        onUpdate: e => direction = e.direction * -1
       },
       x: "-500px",
-    });
+    })
     requestAnimationFrame(animate);
-  }, []);
+  }, [])
 
   const animate = () => {
-    if (xPercent < -100) {
+    if(xPercent < -100){
       xPercent = 0;
-    } else if (xPercent > 0) {
+    }
+    else if(xPercent > 0){
       xPercent = -100;
     }
-    gsap.set(firstText.current, { xPercent: xPercent });
-    gsap.set(secondText.current, { xPercent: xPercent });
+    gsap.set(firstText.current, {xPercent: xPercent})
+    gsap.set(secondText.current, {xPercent: xPercent})
     requestAnimationFrame(animate);
-    xPercent += 0.05 * direction;
-  };
+    xPercent += 0.075 * direction;
+  }
 
   return (
     <main className="relative flex h-[100vh] overflow-hidden bg-black">
+      
       <div className="sliderContainer">
         <div ref={slider} className="slider">
-          <p ref={firstText}>Pradhive ‧ Developer ‧ Designer ‧ </p>
-          <p ref={secondText}>Pradhive ‧ Developer ‧ Designer ‧ </p>
+          <p ref={firstText}>Pradhive ‧ Developer ‧ Designer  ‧ </p>
+          <p ref={secondText}>Pradhive  ‧ Developer ‧ Designer  ‧ </p>
         </div>
       </div>
     </main>
-  );
+  )
 }
